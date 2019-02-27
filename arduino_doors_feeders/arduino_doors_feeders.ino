a1b954ae26ee0a2652c8cad1af321132580016c6
uwlodkowska <u.wlodkowska@nencki.gov.pl>
2019-02-27 17:41:34 +0100

    dodanie kodu do zarzádzania ardui9no sterujácego drzwiczkami

---
+++ arduino_doors_feeders/arduino_doors_feeders.ino
@@ -0,0 +1,90 @@
+/* 
+This is a test sketch for the Adafruit assembled Motor Shield for Arduino v2
+It won't work with v1.x motor shields! Only for the v2's with built in PWM
+control
+
+For use with the Adafruit Motor Shield v2 
+---->	http://www.adafruit.com/products/1438
+*/
+
+#include <Wire.h>
+#include <Adafruit_MotorShield.h>
+
+// Create the motor shield object with the default I2C address
+//Adafruit_MotorShield AFMS = Adafruit_MotorShield(); 
+// Or, create it with a different I2C address (say for stacking)
+//Adafruit_MotorShield AFMS2 = Adafruit_MotorShield(0x62); 
+const int shieldsNumber = 2;
+const int motorsNoPerDirection = 4;
+const int motorsPerShield = 2;
+const int openTime = 4000;
+
+int doorIdx;
+
+Adafruit_MotorShield shields[shieldsNumber];
+
+Adafruit_DCMotor *upMotors[motorsNoPerDirection];
+Adafruit_DCMotor *downMotors[motorsNoPerDirection];
+
+  
+
+// Select which 'port' M1, M2, M3 or M4. In this case, M1
+//Adafruit_DCMotor *up1 = AFMS.getMotor(2);
+// You can also make another motor on port M2
+//Adafruit_DCMotor *down1 = AFMS.getMotor(3);
+
+//Adafruit_DCMotor *up2 = AFMS.getMotor(1);
+// You can also make another motor on port M2
+//Adafruit_DCMotor *down2 = AFMS.getMotor(4);
+
+void setup() {
+  Serial.begin(9600);           // set up Serial library at 9600 bps
+ 
+  shields[0] = Adafruit_MotorShield(0x60); 
+  shields[1] = Adafruit_MotorShield(0x62); 
+
+
+  for (int shieldIdx = 0; shieldIdx < shieldsNumber; shieldIdx++)  {
+    upMotors[shieldIdx*motorsPerShield] = shields[shieldIdx].getMotor(2);
+    downMotors[shieldIdx*motorsPerShield] = shields[shieldIdx].getMotor(3);
+    upMotors[shieldIdx*motorsPerShield+1] = shields[shieldIdx].getMotor(1);
+    downMotors[shieldIdx*motorsPerShield+1] = shields[shieldIdx].getMotor(4);
+  }
+
+  //AFMS.begin();  // create with the default frequency 1.6KHz
+  //AFMS.begin(1000);  // OR with a different frequency, say 1KHz
+  for (int shieldIdx = 0; shieldIdx < shieldsNumber; shieldIdx++)  {
+    shields[shieldIdx].begin();
+  }
+  // Set the speed to start, from 0 (off) to 255 (max speed)
+  for (int motorIdx = 0; motorIdx < motorsNoPerDirection; motorIdx++)  {
+    upMotors[motorIdx]->setSpeed(255);
+    downMotors[motorIdx]->setSpeed(255);
+  }
+}
+
+void loop() {
+
+  if (Serial.available()) {
+    doorIdx = Serial.parseInt();
+    if (doorIdx < 0) {
+      doorIdx = -doorIdx-1;
+      downMotors[doorIdx]->run(FORWARD);
+      delay(openTime);  
+      downMotors[doorIdx]->run(RELEASE);     
+    } else {
+      doorIdx--;
+      upMotors[doorIdx]->run(FORWARD);
+      delay(openTime);  
+      upMotors[doorIdx]->run(RELEASE);  
+    }
+  }
+  
+//  up1->run(FORWARD);
+//  delay(3000);
+//  up1->run(RELEASE);
+//  down1->run(FORWARD);
+//  delay(3000);
+//  down1->run(RELEASE);
+ 
+}
