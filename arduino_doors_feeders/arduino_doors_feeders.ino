2660c6de383cf7e0276f885d0912a680394a24a1
uwlodkowska <u.wlodkowska@nencki.gov.pl>
2019-02-27 17:49:16 +0100

    poczatek programowania arduino do drzwiczek i podajnikow

--- arduino_doors_feeders/arduino_doors_feeders.ino
+++ arduino_doors_feeders/arduino_doors_feeders.ino
@@ -17,6 +17,7 @@ For use with the Adafruit Motor Shield v2
 const int shieldsNumber = 2;
 const int motorsNoPerDirection = 4;
 const int motorsPerShield = 2;
+const int feedersQ = 2;
 const int openTime = 4000;
 
 int doorIdx;
@@ -25,34 +26,25 @@ Adafruit_MotorShield shields[shieldsNumber];
 
 Adafruit_DCMotor *upMotors[motorsNoPerDirection];
 Adafruit_DCMotor *downMotors[motorsNoPerDirection];
-
-  
-
-// Select which 'port' M1, M2, M3 or M4. In this case, M1
-//Adafruit_DCMotor *up1 = AFMS.getMotor(2);
-// You can also make another motor on port M2
-//Adafruit_DCMotor *down1 = AFMS.getMotor(3);
-
-//Adafruit_DCMotor *up2 = AFMS.getMotor(1);
-// You can also make another motor on port M2
-//Adafruit_DCMotor *down2 = AFMS.getMotor(4);
+Adafruit_DCMotor *feeders[feedersQ];
 
 void setup() {
   Serial.begin(9600);           // set up Serial library at 9600 bps
  
-  shields[0] = Adafruit_MotorShield(0x60); 
-  shields[1] = Adafruit_MotorShield(0x62); 
+  shields[0] = Adafruit_MotorShield(0x61); 
+  shields[1] = Adafruit_MotorShield(0x64); 
 
 
-  for (int shieldIdx = 0; shieldIdx < shieldsNumber; shieldIdx++)  {
+  for (int shieldIdx = 0; shieldIdx < shieldsNumber-1; shieldIdx++)  {
     upMotors[shieldIdx*motorsPerShield] = shields[shieldIdx].getMotor(2);
     downMotors[shieldIdx*motorsPerShield] = shields[shieldIdx].getMotor(3);
     upMotors[shieldIdx*motorsPerShield+1] = shields[shieldIdx].getMotor(1);
     downMotors[shieldIdx*motorsPerShield+1] = shields[shieldIdx].getMotor(4);
   }
 
-  //AFMS.begin();  // create with the default frequency 1.6KHz
-  //AFMS.begin(1000);  // OR with a different frequency, say 1KHz
+  upMotors[motorsPerShield] = shields[shieldsNumber-1].getMotor(2);
+  downMotors[motorsPerShield] = shields[shieldsNumber-1].getMotor(1);
+
   for (int shieldIdx = 0; shieldIdx < shieldsNumber; shieldIdx++)  {
     shields[shieldIdx].begin();
   }
